2274e53050284dedfa5b2a07035bf556b462c213
Yến Ngân <yennan275@gmail.com>
2025-03-19 14:30:28 +0700

    Delete MyApp.py

--- MyApp.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from Api.Transaction import MainWindowEx
-# from Api.CategoryEx import MainWindowExt
-app=QApplication([])
-myWindow=MainWindowEx()
-myWindow.setupUi()
-myWindow.show()
-app.exec()
